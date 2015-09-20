4c84ff1d88f29175b47460807b41653d030e87f8
Brett Buddin <brett@buddin.us>
2015-09-20 15:55:14 -0400

    Deploy master

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 set :application, 'oceano'
 set :repo_url, 'git@github.com:wearethescenery/oceano-rails.git'
-set :branch, 'upstart'
+set :branch, 'master'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
