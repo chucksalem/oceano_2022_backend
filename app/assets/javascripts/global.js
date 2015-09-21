f65c38ac3ef6163fa03a52b5694a3c27cb66336b
Andy Rossi <aroccorossi@gmail.com>
2015-09-21 11:14:30 -0400

    second date field updates after first date set

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,3 +1,18 @@
 $(function(){
-  $('.datepicker').pikaday({ firstDay: 1, format: 'M/D/YYYY' });
+  
+  var startPicker = new Pikaday({ field: document.getElementById('start_date'), firstDay: 1, format: 'M/D/YYYY' });
+  var endPicker = new Pikaday({ field: document.getElementById('end_date'), firstDay: 1, format: 'M/D/YYYY' });
+
+  // Update the second date field based on the first field's selected date
+  $('#start_date').on("change", function(e) {
+    
+    var dateString = e.currentTarget.value;
+    var startDate = moment(dateString, 'M/D/YYYY');
+
+    // adds one day to selected start date
+    var nextDate = startDate.add(1, 'd');
+
+    // update the second field
+    endPicker.setMoment(moment(nextDate, 'M/D/YYYY'));
+  });
 });
