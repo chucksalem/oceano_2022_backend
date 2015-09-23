66df2e199fe5699fccce97ef619885c8e8e15243
Brett Buddin <brett@buddin.us>
2015-09-22 22:15:29 -0400

    Eliminate past dates.

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,11 +1,22 @@
 $(function(){
-  
-  var startPicker = new Pikaday({ field: document.getElementById('start_date'), firstDay: 1, format: 'M/D/YYYY' });
-  var endPicker = new Pikaday({ field: document.getElementById('end_date'), firstDay: 1, format: 'M/D/YYYY' });
+
+  var startPicker = new Pikaday({
+      field: document.getElementById('start_date'),
+      firstDay: 1,
+      minDate: moment().toDate(),
+      format: 'M/D/YYYY'
+  });
+
+  var endPicker = new Pikaday({
+      field: document.getElementById('end_date'),
+      firstDay: 1,
+      minDate: moment().toDate(),
+      format: 'M/D/YYYY'
+  });
 
   // Update the second date field based on the first field's selected date
   $('#start_date').on("change", function(e) {
-    
+
     var dateString = e.currentTarget.value;
     var startDate = moment(dateString, 'M/D/YYYY');
 
