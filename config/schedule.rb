9e28f902e1141175f5d251899d92895d14ff8c19
Brett Buddin <brett@buddin.us>
2015-11-22 12:33:32 -0500

    Fix schedule command.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,4 @@
-set :command_with_env, "bash -l -c ':job'"
-
 every :hour do
-  command_with_env "/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties"
-  command_with_env "/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather"
+  command "bash -l -c '/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties'"
+  command "bash -l -c '/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather'"
 end
