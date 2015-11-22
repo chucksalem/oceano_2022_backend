b7523184839caae5183a03606be0b968995b97bc
Brett Buddin <brett@buddin.us>
2015-11-21 19:08:39 -0500

    More deployment weirdness

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
+set :command_with_env, "bash -l -c ':job'"
+
 every :hour do
-  rake 'oceano:cache:properties'
-  rake 'oceano:cache:weather'
+  command_with_env "/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties"
+  command_with_env "/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather"
 end
