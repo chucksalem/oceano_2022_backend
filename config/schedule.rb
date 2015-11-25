3255833b31a851bdeb9ac22a7b19b0c3b9a13961
Brett Buddin <brett@buddin.us>
2015-11-24 21:51:42 -0500

    Relative to this current file then. Rails isn't available.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
+project_dir = File.expand_path(File.dirname(__FILE__) + "/..")
+
 every :hour do
-  command "bash -l -c 'cd #{Rails.root} && /home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties'"
-  command "bash -l -c 'cd #{Rails.root} && /home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather'"
+  command "bash -l -c 'cd #{project_dir} && /home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties'"
+  command "bash -l -c 'cd #{project_dir} && /home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather'"
 end
