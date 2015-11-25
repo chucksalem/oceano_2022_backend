34a0cdbcabedd2b2f1e43e1f4745c9c861ed052d
Brett Buddin <brett@buddin.us>
2015-11-24 21:49:36 -0500

    Need to move to the directory before running this.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every :hour do
-  command "bash -l -c '/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties'"
-  command "bash -l -c '/home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather'"
+  command "bash -l -c 'cd #{Rails.root} && /home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:properties'"
+  command "bash -l -c 'cd #{Rails.root} && /home/#{ENV['USER']}/.rbenv/shims/bundle exec rake oceano:cache:weather'"
 end
