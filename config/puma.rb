86b11300b1228c5c8025b4a12e6459907f9506d1
chris <chr1s.g@yahoo.com>
2021-01-13 13:47:07 -0600

    deleted swaths code commented

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,3 @@
-# threads 0,8
-# workers 2
-# preload_app!
-
-# if ENV['RAILS_ENV'] == 'development'
-#   bind "tcp://0.0.0.0:3000"
-#   daemonize false
-# else
-#   bind "unix://tmp/puma.sock"
-#   daemonize true
-# end
-
-# pidfile 'tmp/pids/puma.pid'
-
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
