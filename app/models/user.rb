04f249c37b8e5abfc491dca5e9d2bfd7ab316192
Julieta Dubra <julietadubra@neocoast.com>
2022-07-29 11:09:04 -0300

    fix: changes PR

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :registerable, :recoverable, :rememberable, :confirmable, :lockable,
-  # :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :validatable
 end
