82220aa5ec692a8606fd9d6f31c536b660793baf
Julieta Dubra <julietadubra@neocoast.com>
2022-07-29 11:09:58 -0300

    fix: changes PR

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,6 @@
 class Recommendation < ApplicationRecord
   def self.get_all()
-    codes       = Recommendation.all()
+    codes       = Recommendation.all
     units       = codes.map { |k| UnitRepository.get(k.unit_id) }
   end
 end
