37713a71a8342507209f1894d453cc3dafe339d5
jdubra <92731151+jdubra@users.noreply.github.com>
2022-08-09 14:27:12 -0300

    feat: mailgun integration (#13)
    * feat: mailgun integration
    
    * fix: changes PR
    
    Co-authored-by: Julieta Dubra <julietadubra@neocoast.com>

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module OceanoRails
     config.action_mailer.delivery_method = :mailgun
     config.action_mailer.mailgun_settings = {
       api_key: ENV['MAILGUN_API_KEY'],
-      domain: 'mg.oceano-rentals.com'
+      domain: ENV['MAILGUN_BASE_URL']
     }
   end
 end
