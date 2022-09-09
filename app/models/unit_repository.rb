ed4929418b0985c924f7fe9e70f94b335dc12e74
jdubra <92731151+jdubra@users.noreply.github.com>
2022-09-09 15:31:37 -0300

    feat: nearby properties are actually nearby (#20)
    * feat: nearby properties are actually nearby
    
    * fix: changes PR
    
    * fix: variable name change
    
    * feat: table filters (#21)
    
    * feat: table filters
    
    * fix: add filter to search as well
    
    * fix: changes PR
    
    Co-authored-by: Julieta Dubra <julietadubra@neocoast.com>
    
    Co-authored-by: Julieta Dubra <julietadubra@neocoast.com>

--- app/models/unit_repository.rb
+++ app/models/unit_repository.rb
@@ -85,4 +85,37 @@ class UnitRepository
   def self.redis
     RedisClient
   end
+
+  def self.closest_units(amount, loc, except: [])
+    except_keys = except.map { |code| "units:#{code}" }
+    all         = redis.keys('units:*') - except_keys
+    distances = []
+    all.each do |key|
+      unit = get(key.sub('units:', ''))
+      value = {
+        unit: unit,
+        distance: distance(loc, [unit.position.latitude, unit.position.longitude])
+      }
+      distances.push(value)
+    end
+    distances = distances.sort_by { |value| value[:distance] }
+    units = distances.map { |value| value[:unit]}
+    units.slice(0, amount)
+  end
+
+  def self.distance(loc1, loc2)
+    rad_per_deg = Math::PI/180
+    rm         = 6371 * 1000
+  
+    dlat_rad    = (loc2[0]-loc1[0]) * rad_per_deg
+    dlon_rad    = (loc2[1]-loc1[1]) * rad_per_deg
+  
+    lat1_rad    = loc1.map {|i| i * rad_per_deg }.first
+    lat2_rad    = loc2.map {|i| i * rad_per_deg }.first
+  
+    a           = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c           = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+  
+    rm * c # Distance in meters
+  end
 end
