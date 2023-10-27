a0a8ef0b568bff4043d6256308e0828bb5126d64
Granit Haxhaj <hax.granit@gmail.com>
2023-10-28 03:00:14 +0500

    OR-55 | Hotfix FE Image

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker stop oceano_2022_backend-frontend-1
 docker rm oceano_2022_backend-frontend-1
 docker rmi chucksalem/oceano-frontend
 docker-compose up --build --detach
-docker exec -it rails /bin/sh
-bundle exec rake oceano:cache:properties
-exit
+# docker exec -it rails /bin/sh
+# bundle exec rake oceano:cache:properties
+# exit
