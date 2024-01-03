7935b21668d96bf649ff657c1a91acb078196b91
Granit Haxhaj <hax.granit@gmail.com>
2024-01-03 17:17:18 -0500

    revert.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,10 +16,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'www.oceano-rentals.com'
-    resource '*', 
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
