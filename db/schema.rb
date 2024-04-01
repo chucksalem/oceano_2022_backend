079b37d12a8cf22f0d37f7a3b9ebc1fa323b807a
frankgzz009 <frankgzzwork@gmail.com>
2024-04-02 02:52:19 +0500

    schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_29_074902) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_01_191414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_29_074902) do
     t.integer "stars"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
