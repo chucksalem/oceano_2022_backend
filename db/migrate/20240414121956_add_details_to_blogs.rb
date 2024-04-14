0d2ab4191073b7f8112322c6b0441ad4653448a1
frankgzz009 <frankgzzwork@gmail.com>
2024-04-14 18:16:37 +0500

    removed duplicate columns

--- db/migrate/20240414121956_add_details_to_blogs.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToBlogs < ActiveRecord::Migration[7.0]
-  def change
-    add_column :blogs, :slug, :string
-    add_column :blogs, :meta_desc, :text
-    add_column :blogs, :meta_title, :string
-  end
-end
