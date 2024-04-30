5847dcfe759a7bfb0f1173d81da925f194028595
frankgzz009 <frankgzzwork@gmail.com>
2024-04-30 21:20:57 +0500

    sidekiq and cron jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,13 @@
+sidekiq_config = { url: ENV['REDIS_URL'] }
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
+
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file) && Sidekiq.server?
