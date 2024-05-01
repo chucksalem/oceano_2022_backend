8c19ef4da0f3dcab862f326db021a70a5095fcdf
frankgzz009 <frankgzzwork@gmail.com>
2024-05-01 12:06:05 +0500

    production deployment script updated

--- build-production.sh
+++ build-production.sh
@@ -1,7 +1,5 @@
 cd /root/apps/oceano_2022_backend
-git pull origin rc-production -f
 docker-compose down
-docker stop oceano_2022_backend_frontend_1
-docker rm oceano_2022_backend_frontend_1
+git pull origin rc-production -f
 docker rmi chucksalem/oceano-frontend:rc_prod
 docker-compose up --build --detach
